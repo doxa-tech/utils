5e24b3f5acbb2bde8694f2f682b7c35e4f06bfbb
Noémien Kocher <nkcr.je@gmail.com>
2017-02-13 11:12:06 +0100

    Create deploystatic-cmd.sh

---
+++ deploystatic-cmd.sh
@@ -0,0 +1,13 @@
+# Update nginx following directives:
+# - remove "default_server"
+# - update "root"
+# - update "server_name"
+
+# /var/www/bati.ch/public_html
+curl https://raw.githubusercontent.com/JS-Tech/utils/master/deploystatic.sh | bash -s bati.ch https://github.com/nkcr/bati.ch.git jstech
+
+# /var/www/ma-sagefeme.ch/public_html
+curl https://raw.githubusercontent.com/JS-Tech/utils/master/deploystatic.sh | bash -s ma-sagefemme.ch https://github.com/JS-Tech/ma-sagefemme.ch.git jstech
+
+# /var/www/microglobal.ch/public_html
+curl https://raw.githubusercontent.com/JS-Tech/utils/master/deploystatic.sh | bash -s microglobal.ch https://github.com/khcr/microglobal.git jstech
