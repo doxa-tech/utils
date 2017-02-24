87d5fead66f072bcad3895ec9d872c8ae55e042e
Noémien Kocher <nkcr.je@gmail.com>
2017-02-24 14:51:29 +0100

    Create deploystatic-cmd-preview.sh

---
+++ deploystatic-cmd-preview.sh
@@ -0,0 +1,5 @@
+# /var/www/preview.romont.eebulle.ch/public_html
+curl https://raw.githubusercontent.com/JS-Tech/utils/master/deploystatic.sh | bash -s preview.romont.eebulle.ch https://github.com/JS-Tech/eebulle.romont.git eebulle
+
+# /var/www/preview.fribourg.eebulle.ch/public_html
+curl https://raw.githubusercontent.com/JS-Tech/utils/master/deploystatic.sh | bash -s preview.fribourg.eebulle.ch https://github.com/JS-Tech/eebulle.fribourg.git eebulle
